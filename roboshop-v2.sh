a35ddd3e59f3cb1f8a75260857904fba3f3b283f
vinay-devops-12 <vinaypayyavula9@gmail.com>
2026-06-26 17:33:51 +0530

    practice

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -3,8 +3,8 @@
 #export PATH=$PATH:/usr/local/bin
 
 AMI_ID="ami-0220d79f3f480ecf5"
-ZONE_ID="Z07086101C1CVP7AT2UK4" # replace with your zone ID
-DOMAIN_NAME="daws90s.shop" # replace with your domain name
+ZONE_ID="Z008852933HNZNSM0V91" # replace with your zone ID
+DOMAIN_NAME="daws-90s.shop" # replace with your domain name
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
